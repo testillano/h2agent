83e181832dc675b8cf39870b1e0551b5ac12aff1
Eduardo Ramos Testillano (ert) <eduardo.ramos.testillano@gmail.com>
2021-06-20 23:52:41 +0200

    Improve ssl utilities
    Change-Id: I197208ce6bb8dcb2c821dced28664746a78ab085

---
+++ tools/ssl/create_self-signed_certificate.sh
@@ -0,0 +1,28 @@
+#!/bin/bash
+# Courtesy of Jesús Gallinal Moreno
+
+SUBJ_common="/C=ES/ST=Madrid/L=Madrid/O=Security/OU=IT Department/CN="
+SUBJ="${SUBJ_common}www.teslayout.com"
+KEY_SIZE=2048
+PASSWORD="hello"
+DAYS=365
+KEY_FILE="my_private_key.pem"
+CRT_FILE="my_public_key_certificate.crt"
+
+openssl req -newkey rsa:${KEY_SIZE} -keyout "${KEY_FILE}" -passout pass:"${PASSWORD}" -subj "${SUBJ}" -out "${CRT_FILE}" -x509 -days ${DAYS}
+
+cat << EOF
+
+  You could start the agent in this way:
+
+  $> ./h2agent -l Debug --verbose --server-key ${KEY_FILE} --server-crt ${CRT_FILE} --secure-admin --server-key-password ${PASSWORD} &
+  $> curl --insecure --http2-prior-knowledge https://localhost:8074/provision/v1/server-matching
+
+  - or -
+
+  Open firefox and accept the risk:
+
+  $> firefox https://localhost:8074/provision/v1/server-matching
+
+EOF
+
