d4b31dbefd062b42be7832e07a470bacbd589fd7
Eduardo Ramos Testillano (ert) <eduardo.ramos.testillano@gmail.com>
2021-07-19 20:55:33 +0200

    Modify resources fixture to improve files struct
    Glob resource files recursively and change provisions
    references in the tests to improve the organization
    of files.
    
    Also, improve pytest.ini

--- ct/src/transform/filter_test.py
+++ ct/src/transform/filter_test.py
@@ -15,7 +15,7 @@ def test_000_cleanup(admin_cleanup):
 def test_001_append(admin_provision, h2ac_traffic):
 
   # Provision
-  admin_provision("provision.transform.filter_test.Append.json")
+  admin_provision("transform/filter_test/provision.Append.json")
 
   # Traffic
   response = h2ac_traffic.get("/app/v1/foo/bar/1")
@@ -28,7 +28,7 @@ def test_001_append(admin_provision, h2ac_traffic):
 def test_002_prepend(admin_provision, h2ac_traffic):
 
   # Provision
-  admin_provision("provision.transform.filter_test.Prepend.json")
+  admin_provision("transform/filter_test/provision.Prepend.json")
 
   # Traffic
   response = h2ac_traffic.get("/app/v1/foo/bar/1")
@@ -41,7 +41,7 @@ def test_002_prepend(admin_provision, h2ac_traffic):
 def test_003_appendVar(admin_provision, h2ac_traffic):
 
   # Provision
-  admin_provision("provision.transform.filter_test.AppendVar.json")
+  admin_provision("transform/filter_test/provision.AppendVar.json")
 
   # Traffic
   response = h2ac_traffic.get("/app/v1/foo/bar/1")
@@ -54,7 +54,7 @@ def test_003_appendVar(admin_provision, h2ac_traffic):
 def test_004_prependVar(admin_provision, h2ac_traffic):
 
   # Provision
-  admin_provision("provision.transform.filter_test.PrependVar.json")
+  admin_provision("transform/filter_test/provision.PrependVar.json")
 
   # Traffic
   response = h2ac_traffic.get("/app/v1/foo/bar/1")
@@ -67,7 +67,8 @@ def test_004_prependVar(admin_provision, h2ac_traffic):
 def test_005_multiply(admin_provision, h2ac_traffic):
 
   # Provision
-  admin_provision("provision.transform.filter_test.Multiply.json")
+  admin_provision("transform/filter_test/provision.Multiply.json")
+  #admin_provision("transform/filter_test/provision.Multiply.json")
 
   # Traffic
   response = h2ac_traffic.get("/app/v1/foo/bar/1")
@@ -81,7 +82,7 @@ def test_005_multiply(admin_provision, h2ac_traffic):
 def test_006_sum(admin_provision, h2ac_traffic):
 
   # Provision
-  admin_provision("provision.transform.filter_test.Sum.json")
+  admin_provision("transform/filter_test/provision.Sum.json")
 
   # Traffic
   response = h2ac_traffic.get("/app/v1/foo/bar/1")
@@ -95,7 +96,7 @@ def test_006_sum(admin_provision, h2ac_traffic):
 def test_007_regexcapture(admin_provision, h2ac_traffic):
 
   # Provision
-  admin_provision("provision.transform.filter_test.RegexCapture.json")
+  admin_provision("transform/filter_test/provision.RegexCapture.json")
 
   # Traffic
   response = h2ac_traffic.get("/app/v1/foo/bar/1")
@@ -108,7 +109,7 @@ def test_007_regexcapture(admin_provision, h2ac_traffic):
 def test_008_regexcapturemultiple(admin_provision, h2ac_traffic):
 
   # Provision
-  admin_provision("provision.transform.filter_test.RegexCapture-Multiple.json")
+  admin_provision("transform/filter_test/provision.RegexCapture-Multiple.json")
 
   # Traffic
   response = h2ac_traffic.get("/app/v1/foo/bar/1")
@@ -121,7 +122,7 @@ def test_008_regexcapturemultiple(admin_provision, h2ac_traffic):
 def test_009_regexreplace(admin_provision, h2ac_traffic):
 
   # Provision
-  admin_provision("provision.transform.filter_test.RegexReplace.json")
+  admin_provision("transform/filter_test/provision.RegexReplace.json")
 
   # Traffic
   response = h2ac_traffic.get("/app/v1/foo/bar/1")
@@ -133,7 +134,7 @@ def test_009_regexreplace(admin_provision, h2ac_traffic):
 def test_010_conditionVariable(admin_provision, h2ac_traffic):
 
   # Provision
-  admin_provision("provision.transform.filter_test.ConditionVar.json")
+  admin_provision("transform/filter_test/provision.ConditionVar.json")
 
   # Traffic
   response = h2ac_traffic.get("/app/v1/foo/bar/1")
