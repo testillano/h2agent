109786a154862f040744aa0e7dc20fa64c9748d4
Eduardo Ramos Testillano (ert) <eduardo.ramos.testillano@gmail.com>
2021-10-24 22:41:26 +0200

    Add initial unit-tests and coverage analysis
    - Added minimal UT for json schema class.
    - Dockerfile and script to ease coverage analysis.
    - Workflow for coverage analysis and upload to codecov.
    
      Coverage badge shall be written in README when UT is ready:
    
      [![codecov](https://codecov.io/gh/testillano/h2agent/branch/master/graph/badge.svg)](https://codecov.io/gh/testillano/h2agent)

---
+++ ut/jsonSchema/jsonSchema_test.cpp
@@ -0,0 +1,39 @@
+#include <JsonSchema.hpp>
+#include <AdminSchemas.hpp>
+
+#include <gmock/gmock.h>
+#include <gtest/gtest.h>
+
+
+const nlohmann::json ServerMatchingJson = R"({"algorithm":"FullMatching"})"_json;
+const nlohmann::json ServerProvisionJson = R"({"requestMethod": "GET", "requestUri": "/foo/bar", "responseCode": 200})"_json;
+
+class jsonSchema_test : public ::testing::Test
+{
+  public:
+    h2agent::jsonschema::JsonSchema json_schema_;
+};
+
+TEST_F(jsonSchema_test, validSchema1)
+{
+    ASSERT_TRUE(jsonSchema_test::json_schema_.setSchema(h2agent::adminSchemas::server_matching));
+    ASSERT_TRUE(jsonSchema_test::json_schema_.isAvailable());
+    EXPECT_EQ(jsonSchema_test::json_schema_.getSchema(), h2agent::adminSchemas::server_matching);
+    ASSERT_TRUE(jsonSchema_test::json_schema_.validate(ServerMatchingJson));
+}
+
+TEST_F(jsonSchema_test, validSchema2)
+{
+    ASSERT_TRUE(jsonSchema_test::json_schema_.setSchema(h2agent::adminSchemas::server_provision));
+    ASSERT_TRUE(jsonSchema_test::json_schema_.isAvailable());
+    EXPECT_EQ(jsonSchema_test::json_schema_.getSchema(), h2agent::adminSchemas::server_provision);
+    ASSERT_TRUE(jsonSchema_test::json_schema_.validate(ServerProvisionJson));
+}
+
+TEST_F(jsonSchema_test, invalidSchema)
+{
+    nlohmann::json not_an_schema{};
+    ASSERT_FALSE(jsonSchema_test::json_schema_.setSchema(not_an_schema));
+    ASSERT_FALSE(jsonSchema_test::json_schema_.isAvailable());
+}
+
