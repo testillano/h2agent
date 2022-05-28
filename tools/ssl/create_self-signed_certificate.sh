c3cce391bada8440016d3198f82842ebf0a6ee1e
Eduardo Ramos Testillano (ert) <eduardo.ramos.testillano@gmail.com>
2022-05-29 01:14:06 +0200

    Add SSL/TLS section to README file
    Also, ssl helper script is fixed with updated arguments.

--- tools/ssl/create_self-signed_certificate.sh
+++ tools/ssl/create_self-signed_certificate.sh
@@ -15,7 +15,7 @@ cat << EOF
 
   You could start the agent in this way:
 
-  $> ./h2agent -l Debug --verbose --server-key ${KEY_FILE} --server-crt ${CRT_FILE} --secure-admin --server-key-password ${PASSWORD} &
+  $> ./h2agent -l Debug --verbose --traffic-server-key ${KEY_FILE} --traffic-server-crt ${CRT_FILE} --secure-admin --traffic-server-key-password ${PASSWORD} &
   $> curl --insecure --http2-prior-knowledge https://localhost:8074/admin/v1/server-matching
 
   - or -
