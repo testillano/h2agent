7789a9ec5b896de8207cf0ac7928853516d145b0
Eduardo Ramos Testillano (ert) <eduardo.ramos.testillano@gmail.com>
2024-09-11 13:32:47 +0200

    Increase ssl certificates security
    To avoid" SSLError: [SSL: EE_KEY_TOO_SMALL] ee key too small"
    error, we will set 2048 instead of 1024 bits for RSA.

--- tools/ssl/create_all.sh
+++ tools/ssl/create_all.sh
@@ -10,14 +10,14 @@ openssl genrsa -des3 -out ca.key 4096
 openssl req -new -x509 -days 365 -key ca.key -out ca.crt -subj "${SUBJ_CA}"
 
 # Create the Server Key, CSR, and Certificate
-openssl genrsa -des3 -out server.key 1024
+openssl genrsa -des3 -out server.key 2048
 openssl req -new -key server.key -out server.csr -subj "${SUBJ}"
 
 # We're self signing our own server cert here.  This is a no-no in production.
 openssl x509 -req -days 365 -in server.csr -CA ca.crt -CAkey ca.key -set_serial 01 -out server.crt
 
 # Create the Client Key and CSR
-openssl genrsa -des3 -out client.key 1024
+openssl genrsa -des3 -out client.key 2048
 openssl req -new -key client.key -out client.csr -subj "${SUBJ}"
 
 # Sign the client certificate with our CA cert.  Unlike signing our own server cert, this is what we want to do.
