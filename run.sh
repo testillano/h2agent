3e943bb47dbefd0906d974420c4e9a818f5af2ce
Eduardo Ramos Testillano (ert) <eduardo.ramos.testillano@gmail.com>
2025-06-07 00:58:14 +0200

    Improve benchmark and run scripts
    Also, improve output for do_curl in case of trace call.

--- run.sh
+++ run.sh
@@ -6,11 +6,23 @@ RUN_AS="-u $(id -u)"
 img_variant=
 [ -n "${HTTP1_ENABLED}" ] && img_variant=_http1 && RUN_AS= # to allow creation of nghttpx configuration
 
+# Network (i.e.: '--network=back_tier -p 8000:8000 -p 8074:8074')
+NETWORK=${NETWORK:-"--network=host"}
+
 # Recommended for 'benchmark/start.sh' (comment to use only provided arguments):
 BENCH=(--verbose --traffic-server-worker-threads 5 --prometheus-response-delay-seconds-histogram-boundaries "100e-6,200e-6,300e-6,400e-6,1e-3,5e-3,10e-3,20e-3")
 
+## Rename to 'h2agent-<unique name>':
+#NAME=
+#(
+#sleep 5
+#name=$(docker ps --filter ancestor=ghcr.io/testillano/h2agent --format "{{.Names}}")
+#docker rename ${name} h2agent.${name}
+#) &
+NAME="--name h2agent"
+
 # Run './build.sh --auto' to have docker image available:
-docker run ${RUN_AS} --rm -it --network=host ${XTRA_ARGS} ghcr.io/testillano/h2agent${img_variant}:${H2A_TAG} ${BENCH[*]} $@
+docker run ${RUN_AS} --rm -it ${NAME} ${NETWORK} ${XTRA_ARGS} ghcr.io/testillano/h2agent${img_variant}:${H2A_TAG} ${BENCH[*]} $@
 
 # Using ctr-tools:
 #
